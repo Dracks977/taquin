d71abb1b234f5c3aeea6b2fdd9afb637e5914909
Vincent Vieira <2163284+vincent-vieira@users.noreply.github.com>
2020-12-09 15:25:17 +0100

    Update Grid.js

--- src/Grid.js
+++ src/Grid.js
@@ -1,12 +1,12 @@
 import React from 'react'
 const Cell = React.lazy(() => import('./Cell'))
 
-function Grid({ grid }) {
+function Grid({ grid, onClick }) {
   return (
      <div className="bord">
    		{[...Array(4).keys().map((number, yPos) => {
 	return <div key={number}  className="board-row">
-		{[...Array(4).keys()].map((number, xPos) => (<Cell value={grid[yPos][xPos]} onClick={() => props.onClick(xPos, yPos)}/>)}
+		{[...Array(4).keys()].map((number, xPos) => (<Cell value={grid[yPos][xPos]} onClick={() => onClick(xPos, yPos)}/>)}
 	</div>
 })}
     </div>
