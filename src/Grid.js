69d3d777db15384e1ab134140112f1ec18f47906
Dracks <eclat.nathan@gmail.com>
2020-12-09 15:42:29 +0100

    change

--- src/Grid.js
+++ src/Grid.js
@@ -2,7 +2,7 @@ import React from 'react'
 const Cell = React.lazy(() => import('./Cell'))
 
 function Grid(props) {
-const listItems = props.grid.map((number, i) => {
+const listItems = props.tab.map((number, i) => {
 	return <div key={number}  className="board-row">
 		<Cell value={number} x={i} onClick={props.onClick}/>
 	</div>
